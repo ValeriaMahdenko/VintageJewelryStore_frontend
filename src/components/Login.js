223728ba4ba5c070d36a5162b3057742295e9589
ValeriaMahdenko <vumahdenko6@gmail.com>
2021-10-16 18:20:06 +0300

    create login/logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,10 @@ export class Login extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { Email: "", Password: "", Redirect: false };
+    this.state = { Email: "", Password: ""};
+
+    //console.log(localStorage["Token"]);
+    //console.log(localStorage["IsLogin"]);
   }
 
   setEmail(em ) {
@@ -25,41 +28,63 @@ export class Login extends Component {
       ).then(response => {localStorage.setItem("Id", response.data.id); localStorage.setItem("IsAdmin", response.data.is_admin); localStorage.setItem("IsLogin", true)}
       )
   }
+  
+  /*componentDidUpdate()
+  {
+    //console.log("not if");
+    if(localStorage["Reload"]){
+      //console.log("if");
+      //document.getElementById("log").click();
+      localStorage.setItem('Reload', false);
+      
+      document.location.reload();
+
+    }
+  }
 
   Signin(e){
+    localStorage.clear();
     e.preventDefault();
       axios.post("http://127.0.0.1:8000/users/login/", {
         "email": this.state.Email,
         "password": this.state.Password,
-    }).then(response => localStorage.setItem("Token", response.data.access))
+    }).then(response => {localStorage.setItem("Token", response.data.access); localStorage.setItem("IsLogin", true)})
 
-    this.GetUserToken();
-
-    this.setState({Redirect: true});
-
-  }
-  
-  Signout(e)
+    localStorage.setItem('Reload', true);
+    //console.log(localStorage["Reload"]);
+    this.forceUpdate();
+    //console.log("after force");
+  }*/
+  Signin(e)
   {
     e.preventDefault();
-    localStorage.setItem("IsLogin", false);
-    localStorage.setItem("Token", "");
-    localStorage.setItem("Id", 0);
-    localStorage.setItem("IsAdmin", false);
+    axios.post("http://127.0.0.1:8000/users/login/", {
+        "email": this.state.Email,
+        "password": this.state.Password,
+    }).then(response => {
+      const token = response.data.access;
+      const IsLogin = true;
 
-    this.GetUserToken();
+      localStorage.setItem('Token', token);
+      localStorage.setItem('IsLogin', IsLogin);
 
-    this.setState({Redirect: true});
+      console.log(localStorage['Token']);
+      console.log(localStorage['IsLogin']);
+
+      this.props.history.push("/")
+      document.location.reload();
+    })
   }
+
   render () {
     return (
       <div>
       <h2>Login Page</h2><br></br>
     <div class="login">
+        <form>
         <p>Not a member?
             <a href="/register">Log up now</a>
         </p>
-        <form id="login">
             <label>Email</label>
             <input type="text" name="UserEmail" id="UserEmail" value={this.state.Email} onChange={e=>this.setEmail(e.target.value)} placeholder="Email" />
             <br></br>
@@ -68,14 +93,11 @@ export class Login extends Component {
             <input type="Password" name="Pass" id="Pass" value={this.state.Password} onChange={e=>this.setPassword(e.target.value)} placeholder="Password"/>
 
             <br></br>
-            <button type="button" name="log" id="log" onClick={e => this.Signin(e)}>Log in</button>
+            <button type="button" name="log" id="log" onClick={e => this.Signin(e)} >Log in</button>
+            
             <br></br>
-
-            <button type="button" name="log" id="logout" onClick={e => this.Signout(e)}>Sign out</button>
-
-         {this.state.Redirect && <Redirect to={"/"}/>}
-        </form>
-
+            </form>
+            
         </div>
     </div>
     );
