c5cb9dcf8155ec82f8f424c109629caadc137ffb
ValeriaMahdenko <vumahdenko6@gmail.com>
2021-10-20 03:10:44 +0300

    add functional for user's cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './style/cart.css'
 import $ from 'jquery'; // <-to import jquery
 import { isJSDocThisTag } from 'typescript';
+import swal from "sweetalert";
 
 export class Cart extends Component {
   static displayName = Cart.name;
@@ -14,7 +15,8 @@ export class Cart extends Component {
       Price: 0,
       Product_name: "",
       Product_image: "",
-      Product_price: ""
+      Product_price: "",
+      Product_id: 0
     };
   }
 
@@ -30,10 +32,69 @@ export class Cart extends Component {
     });
   }
 
+  RemoveProduct(e)
+  {
+    e.preventDefault();
+    var dataID = parseInt(e.target.getAttribute("data-id"));     
+    var dataIndex = parseInt(e.target.getAttribute("data-index"));     
+
+    console.log(dataID)
+    console.log(dataIndex)
+
+    var d = this.state.Products
+    console.log(d)
+    console.log(d[dataIndex])
+    d.splice(dataIndex, 1)
+    console.log(d)
+
+    axios.put(`http://127.0.0.1:8000/orders/${localStorage['IdOrder']}/`, 
+        {
+          "status": "Opened",
+          "selected_products": d,
+        },
+        { headers: {"Authorization" : `Bearer ${localStorage["Token"]}`}})
+        .then(function(response) {
+          swal({
+            title: "Product removed from your cart!",
+            icon: "success",
+            button: 'OK',
+          }).then(function(isConfirm) {
+            if (isConfirm) {
+              window.location.href = "/cart";
+            } 
+          });
+        })
+  }
+
+  ConfirmOrder(e)
+  {
+    e.preventDefault();
+    console.log("okeyyy");
+    axios.put(`http://127.0.0.1:8000/orders/${localStorage['IdOrder']}/`, 
+        {
+          "status": "Completed",
+          "selected_products": this.state.Products,
+        },
+        { headers: {"Authorization" : `Bearer ${localStorage["Token"]}`}})
+        .then(function(response) {
+          localStorage.setItem("IdOrder", '')
+          swal({
+            title: "Your order has been accepted!",
+            icon: "success",
+            button: 'OK',
+          }).then(function(isConfirm) {
+            if (isConfirm) {
+              window.location.href = "/shopList";
+            } 
+          });
+        })
+  }
+
   WatchProducts()
   {
     console.log("in")
     document.getElementById("Cart-Items").innerHTML="";
+    var i = 0;
     this.state.Products.forEach((element) => {
         console.log(element);
         axios.get(`http://127.0.0.1:8000/products/${element.product}/`).then(response => {
@@ -41,7 +102,8 @@ export class Cart extends Component {
             this.setState({
                 Product_name: response.data.name,
                 Product_image: response.data.images[0].image,
-                Product_price: response.data.price
+                Product_price: response.data.price,
+                Product_id: response.data.id
         })
 
         var amount = document.createElement("div")
@@ -50,6 +112,9 @@ export class Cart extends Component {
 
         var button = document.createElement("button")
         button.setAttribute("class", "remove")
+        button.setAttribute("data-Id", this.state.Product_id)
+        button.setAttribute("data-index", i)
+        button.onclick =  (e)=>{this.RemoveProduct(e); }
         button.innerText = "Remove"
 
         var prices = document.createElement("div")
@@ -69,6 +134,7 @@ export class Cart extends Component {
         item.append(img, h, prices)
 
         document.getElementById('Cart-Items').append(item)
+        i++;
     })
     
   })
@@ -94,7 +160,7 @@ export class Cart extends Component {
                 </div>
                 <div class="total-amount">{this.state.Price}</div>
             </div>
-            <button class="button">Checkout</button>
+            <button class="button" onClick={e => this.ConfirmOrder(e)}>Checkout</button>
         </div>
 
     </div>
